0d42de52d15fe9f4b051dc2c63a0a0ee2cfa5a75
YUnagasaka <nagasaka0987@gmail.com>
2019-10-23 05:25:14 +0000

    Some fix

--- kadai-2/index.js
+++ kadai-2/index.js
@@ -1,15 +1,13 @@
-const name = 'sina'
-console.log(name)
+const firstName = 'Yuu'
+const secondName = 'Nagasaka'
+console.log(firstName + secondName)
 
-let name2 = 'yuu'
-name2='yuu'
-console.log(name2)
+const sum = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10
+console.log(sum)
 
-const phrace = "I'm a cat."
-console.log(phrace)
+const hasCat = false
+const hasDog = true
 
-
-const name = 'Mike'
-console.log(typeof name)
-const age = 3
-console.log(typeof age)
+console.log(hasCat || hasDog)
+console.log(hasCat && hasDog)
+console.log(!hasCat && !hasDog)
