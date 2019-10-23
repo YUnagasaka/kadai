16bbca8c39b67e43fc5cbd341ad88d10907299f9
YUnagasaka <nagasaka0987@gmail.com>
2019-10-23 05:18:23 +0000

    Some fix

---
+++ kadai-2/index.js
@@ -0,0 +1,15 @@
+const name = 'sina'
+console.log(name)
+
+let name2 = 'yuu'
+name2='yuu'
+console.log(name2)
+
+const phrace = "I'm a cat."
+console.log(phrace)
+
+
+const name = 'Mike'
+console.log(typeof name)
+const age = 3
+console.log(typeof age)
---
+++ kadai-3/index.js
@@ -0,0 +1,13 @@
+const firstName = 'Yuu'
+const secondName = 'Nagasaka'
+console.log(firstName + secondName)
+
+const sum = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10
+console.log(sum)
+
+const hasCat = false
+const hasDog = true
+
+console.log(hasCat || hasDog)
+console.log(hasCat && hasDog)
+console.log(!hasCat && !hasDog)
