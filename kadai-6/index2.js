b572695208f13c56346112eb7aaadc8c8b363fed
YUnagasaka <nagasaka0987@gmail.com>
2019-12-04 05:26:39 +0000

    課題６を追加

--- kadai-6/index2.js
+++ kadai-6/index2.js
@@ -10,14 +10,14 @@ try {
     console.log(JSON.parse(str1))
 } catch (error) {
     console.log(error)
-    //  abc という文字列は正しい　JSONという理由でパースできない
+    //  abc という文字列は正しい。しかし、JSONではない。"abc"が正しい。
 }
 
 try {
     console.log(JSON.parse(str2))
 } catch (error) {
     console.log(error)
-    //  
+    //  abc という文字列は正しい。しかし、JSONではない。"abc"が正しい。
 }
 
 try {
@@ -31,7 +31,7 @@ try {
     console.log(JSON.parse(str4))
 } catch (error) {
     console.log(error)
-    //  
+    //  JSONではない。
 }
 
 try {
@@ -45,5 +45,5 @@ try {
     console.log(JSON.parse(str6))
 } catch (error) {
     console.log(error)
-    //  
+    //  ''内の部分が正しくない。
 }
