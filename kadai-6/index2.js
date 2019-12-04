b5be8d8ca286715096d2b8c7048bb0f18da98ea5
YUnagasaka <nagasaka0987@gmail.com>
2019-12-04 05:18:07 +0000

    課題7を追加

---
+++ kadai-6/index2.js
@@ -0,0 +1,49 @@
+const str1 = "abc"
+const str2 = "'abc'"
+const str3 = '"abc"'
+const str4 = '{"a": undefined}'
+const str5 = '{"a": 123}'
+const str6 = '{a: 123}'
+
+
+try {
+    console.log(JSON.parse(str1))
+} catch (error) {
+    console.log(error)
+    //  abc という文字列は正しい　JSONという理由でパースできない
+}
+
+try {
+    console.log(JSON.parse(str2))
+} catch (error) {
+    console.log(error)
+    //  
+}
+
+try {
+    console.log(JSON.parse(str3))
+} catch (error) {
+    console.log(error)
+    //  正しい
+}
+
+try {
+    console.log(JSON.parse(str4))
+} catch (error) {
+    console.log(error)
+    //  
+}
+
+try {
+    console.log(JSON.parse(str5))
+} catch (error) {
+    console.log(error)
+    //  正しい
+}
+
+try {
+    console.log(JSON.parse(str6))
+} catch (error) {
+    console.log(error)
+    //  
+}
